0a0f328c85d48fb16bb9ec8e8bf3c59e07198c03
Liang <806446856@qq.com>
2024-11-15 15:47:22 +0800

    first add

---
+++ firstPyCode.py
@@ -0,0 +1,42 @@
+# 打印字符串
+# print('hello world')
+
+# 打印多个字符串
+# print('firstWord','secondWord','thirdWord')
+
+# 打印数字和进行数字计算
+# print(100)
+# print(100+200)
+# print('100 + 200 = ',100+200)
+
+# 打印输入的字符
+# name = input('enter your name :')
+# print('hello',name)
+
+# result = (1024*768)
+# print('1024*768 =',result)
+
+# word = 'i,am "ok"'
+# print(word)
+
+# print('\\\n\\')
+# 结果如下
+# C:\PythonWorkSpace>python firstPyCode.py
+# \
+# \
+
+# r''表示''内部的字符串默认不转义
+# print(r'\\\n\\')
+
+# print('''line1
+# line2
+# line3''')
+
+# print(r'''hello,\n
+#       world''')
+
+# python的赋值策略
+a = '123'
+b = a
+a = '456'
+print(b)
